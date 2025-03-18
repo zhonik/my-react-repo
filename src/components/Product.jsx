f75229ea80064aed3b378a2c468784373a820802
Serhii Krasnozhon <zhonik92@gmail.com>
2025-03-19 00:01:54 +0200

    create repo

---
+++ src/components/Product.jsx
@@ -0,0 +1,9 @@
+export default function Product({ name, imgUrl, price }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width='480' />
+      <p>Price: {price} credits</p>
+    </div>
+  );
+}
